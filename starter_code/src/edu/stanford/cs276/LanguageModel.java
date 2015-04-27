b64e0ee1e0e62271446b512c532e7cd393117ed0
Christoph Wertz <christoph.wertz@gmail.com>
2015-04-26 20:17:25 -0600

    Fix brackets, and calcualte unigrams and bigrams.

--- starter_code/src/edu/stanford/cs276/LanguageModel.java
+++ starter_code/src/edu/stanford/cs276/LanguageModel.java
@@ -9,10 +9,12 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+import edu.stanford.cs276.util.Debug;
 import edu.stanford.cs276.util.Dictionary;
 
 
-public class LanguageModel implements Serializable {
+public class LanguageModel implements Serializable 
+{
 
 	private static LanguageModel lm_;
 	/* Feel free to add more members here.
@@ -22,51 +24,73 @@ public class LanguageModel implements Serializable {
 	 */
 	
 	Dictionary unigram = new Dictionary();
+	Dictionary bigram = new Dictionary();
 	// Do not call constructor directly since this is a Singleton
-	private LanguageModel(String corpusFilePath) throws Exception {
+	private LanguageModel(String corpusFilePath) throws Exception 
+	{
 		constructDictionaries(corpusFilePath);
 	}
 
 
-	public void constructDictionaries(String corpusFilePath)
-			throws Exception {
+	public void constructDictionaries(String corpusFilePath) throws Exception 
+	{
+
+		Debug d = new Debug();
+		d.setDev("dev");
 
 		System.out.println("Constructing dictionaries...");
 		File dir = new File(corpusFilePath);
-		for (File file : dir.listFiles()) {
-			if (".".equals(file.getName()) || "..".equals(file.getName())) {
+		for (File file : dir.listFiles()) 
+		{
+			if (".".equals(file.getName()) || "..".equals(file.getName())) 
+			{
 				continue; // Ignore the self and parent aliases.
 			}
 			System.out.printf("Reading data file %s ...\n", file.getName());
 			BufferedReader input = new BufferedReader(new FileReader(file));
 			String line = null;
-			while ((line = input.readLine()) != null) {
+			while ((line = input.readLine()) != null) 
+			{
 				/*
 				 * Your code here
 				 */
-				unigram.add("cs276");
+				String[] tokens = line.trim().split("\\s+");
+				for (int i=0; i<tokens.length-1; i++)
+				{
+					unigram.add(tokens[i]);
+					bigram.add(tokens[i] + tokens[i+1]);
+				}
+				unigram.add(tokens[tokens.length-1]);
 			}
 			input.close();
 		}
+		d.dev("Unigram term count: " + unigram.termCount());
+		d.dev("Bigram term count: " + bigram.termCount());
 		System.out.println("Done.");
 	}
 	
 	// Loads the object (and all associated data) from disk
-	public static LanguageModel load() throws Exception {
-		try {
-			if (lm_==null){
+	public static LanguageModel load() throws Exception 
+	{
+		try 
+		{
+			if (lm_==null)
+			{
 				FileInputStream fiA = new FileInputStream(Config.languageModelFile);
 				ObjectInputStream oisA = new ObjectInputStream(fiA);
 				lm_ = (LanguageModel) oisA.readObject();
 			}
-		} catch (Exception e){
+		} 
+		catch (Exception e)
+		{
 			throw new Exception("Unable to load language model.  You may have not run build corrector");
 		}
 		return lm_;
 	}
 	
 	// Saves the object (and all associated data) to disk
-	public void save() throws Exception{
+	public void save() throws Exception
+	{
 		FileOutputStream saveFile = new FileOutputStream(Config.languageModelFile);
 		ObjectOutputStream save = new ObjectOutputStream(saveFile);
 		save.writeObject(this);
@@ -74,8 +98,10 @@ public class LanguageModel implements Serializable {
 	}
 	
 	// Creates a new lm object from a corpus
-	public static LanguageModel create(String corpusFilePath) throws Exception {
-		if(lm_ == null ){
+	public static LanguageModel create(String corpusFilePath) throws Exception 
+	{
+		if(lm_ == null )
+		{
 			lm_ = new LanguageModel(corpusFilePath);
 		}
 		return lm_;
